c2236960e0a7cec30c81ab8470e1b53bf317dde2
Anthony Smith <anthony@sticksnleaves.com>
2013-08-29 16:30:05 -0400

    Added push validation endpoint.

--- lib/dirigible/push.rb
+++ lib/dirigible/push.rb
@@ -4,7 +4,17 @@ class Dirigible::Push < Dirigible::Endpoint
   #
   # * A single [Push Object](http://docs.urbanairship.com/reference/api/v3/push.html#push-object)
   # * An array of one or more [Push Objects](http://docs.urbanairship.com/reference/api/v3/push.html#push-object)
+  #
+  # @see http://docs.urbanairship.com/reference/api/v3/push.html#push
   def self.create(params)
     new(Dirigible.post('/push', params))
+  end
+
+  # Accept the same range of payloads as /api/push, but parse
+  # and validate only, without sending any pushes.
+  #
+  # @see http://docs.urbanairship.com/reference/api/v3/push.html#validate
+  def self.validate(params)
+    new(Dirigible.post('/push/validate'))
   end
 end
