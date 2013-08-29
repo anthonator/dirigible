c2954a74213acc2ed1f91bb9543e2bcd0f48c7b4
Anthony Smith <anthony@sticksnleaves.com>
2013-08-29 16:23:53 -0400

    Renamed master_key to master_secret.

--- lib/dirigible/connection.rb
+++ lib/dirigible/connection.rb
@@ -6,7 +6,7 @@ module Dirigible
       options = { ssl: { verify: true }, proxy: proxy }
       Faraday.new(endpoint, options) do |faraday|
         faraday.request(:json)
-        faraday.basic_auth(app_key, master_key)
+        faraday.basic_auth(app_key, master_secret)
         faraday.adapter(http_adapter)
       end
     end
