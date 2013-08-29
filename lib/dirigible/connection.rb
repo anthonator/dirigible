2bec7ad23463fe8c5965c90478f4d06dff69ef07
Anthony Smith <anthony@sticksnleaves.com>
2013-08-29 15:05:18 -0400

    Added configure options to connection.

--- lib/dirigible/connection.rb
+++ lib/dirigible/connection.rb
@@ -3,9 +3,11 @@ module Dirigible
   module Connection
     private
     def connection
-      options = { ssl: { verify: true } }
+      options = { ssl: { verify: true }, proxy: proxy }
       Faraday.new(endpoint, options) do |faraday|
         faraday.request(:json)
+        faraday.basic_auth(app_key, master_key)
+        faraday.adapter(http_adapter)
       end
     end
   end
