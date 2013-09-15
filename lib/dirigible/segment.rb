1eef958fb3a57f0daf0a1ba3eea755a647a3a16a
Anthony Smith <anthony@sticksnleaves.com>
2013-09-15 02:53:43 -0400

    Modified all methods to be class methods.

--- lib/dirigible/segment.rb
+++ lib/dirigible/segment.rb
@@ -3,35 +3,35 @@ class Dirigible::Segment
   # List all of the segments for the application.
   #
   # @see http://docs.urbanairship.com/reference/api/v3/segments.html#segments-information
-  def list
+  def self.list
     Dirigible.get('/segments')
   end
 
   # Fetch information about a particular segment.
   #
   # @see http://docs.urbanairship.com/reference/api/v3/segments.html#segments-information
-  def get(id)
+  def self.get(id)
     Dirigible.get("/segments/#{id}")
   end
 
   # Create a new segment.
   #
   # @see http://docs.urbanairship.com/reference/api/v3/segments.html#segment-creation
-  def create(params)
+  def self.create(params)
     Dirigible.post('/segments', params)
   end
 
   # Change the definition of the segment.
   #
   # @see http://docs.urbanairship.com/reference/api/v3/segments.html#segment-creation
-  def update(id, params)
+  def self.update(id, params)
     Dirigible.put("/segments/#{id}", params)
   end
 
   # Remove the segment.
   #
   # @see http://docs.urbanairship.com/reference/api/v3/segments.html#segment-creation
-  def delete(id)
+  def self.delete(id)
     Dirigible.delete("/segments/#{id}")
   end
 end
